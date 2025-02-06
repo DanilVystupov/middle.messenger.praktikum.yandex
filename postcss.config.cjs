3fa36c076c6b59900ce24977e9a85bb5de13788b
Danil Vystupov <vystupov.d@selectel.com>
2025-02-06 11:41:57 +0300

    Added postcss config and server file

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,14 @@
 module.exports = {
-    plugins: {
-        autoprefixer: {},
+  plugins: {
+    'postcss-mixins': {},
+    'postcss-nested': {},
+    'postcss-simple-vars': {},
+    'postcss-preset-env': {
+      stage: 1,
+      features: {
+        'nesting-rules': true,
+      },
     },
+    autoprefixer: {},
+  },
 };
