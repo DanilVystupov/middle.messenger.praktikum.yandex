1693dfaa2fb6f40bbe88ef7b633391e96fafc21a
Danil Vystupov <vystupov.d@selectel.com>
2025-02-06 12:34:47 +0300

    Remove vite config

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import handlebars from 'vite-plugin-handlebars'
-
-export default defineConfig({
-  plugins: [handlebars()],
-  css: {
-    postcss: 'postcss.config.js' // Подключаем PostCSS
-  }
-})
