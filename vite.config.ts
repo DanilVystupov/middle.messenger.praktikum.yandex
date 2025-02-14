e829a0c217dc5d93dad9a58a8bf09f8fed1fba8e
Danil Vystupov <vystupov.d@selectel.com>
2025-02-14 14:06:12 +0300

    Changed a command npm start

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.',
-  server: {
-    port: 3000,
-    strictPort: true
-  },
+  preview: { port: 3000 },
   build: {
     outDir: 'dist'
   }
